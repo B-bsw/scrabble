5f15a5b2f8b05dfd6aa27f96e1cbf1ee5d1f3a1f
B-bsw <sorawitthassadorn15@gmail.com>
2026-05-11 00:16:46 +0700

    fix: ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <html
             lang="en"
             className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-            data-theme="ocean"
+            data-theme="monochrome"
         >
             <body className="flex min-h-full flex-col">{children}</body>
         </html>
