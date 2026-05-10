fbbf40cefcf41d47d41e5884f54356c7d6be8e28
B-bsw <sorawitthassadorn15@gmail.com>
2026-05-10 15:23:20 +0700

    feat:add heroui and loading

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,16 +1,13 @@
 "use client";
 
+import { Card, Input, Spinner } from "@heroui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function MainPage() {
   const [data, setData] = useState<Record<string, number>>({});
   const [inputWord, setInputWord] = useState<string>("");
-  const [isHasWord, setIsHasWord] = useState<boolean>(false);
-
-  // const isValid = () => {
-  //   setInputWord(data[inputWord] === 1);
-  // };
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = () => {
@@ -21,23 +18,32 @@ export default function MainPage() {
       const fData = axios
         .get(API + "all")
         .then((e) => setData(e.data))
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err))
+        .finally(() => setIsLoading(false));
     };
 
     fetchData();
   }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <Spinner />
+        <span className="ml-2">Loading</span>
+      </>
+    );
+  }
+
   return (
     <>
-      <div>
-        <div>{inputWord.length === 0 ? 'N/A' : inputWord}</div>
-        <input
-          className="outline"
-          onChange={(e) => {
-            setInputWord(e.currentTarget.value.toUpperCase());
-          }}
+      <Card>
+        <div>{inputWord.length === 0 ? "N/A" : inputWord}</div>
+        <Input
+          variant="secondary"
+          onChange={(e) => setInputWord(e.currentTarget.value.toUpperCase())}
         />
-        <div>{data[inputWord] === 1 ? "Has word" : "don't has word"}</div>
-      </div>
+        <div>{data[inputWord] ? "Has word" : "don't has word"}</div>
+      </Card>
     </>
   );
 }
